1babb6a524e79e3ab74be6f4fce4dbbcb03b6234
Matthew Day <daystar.1998@yahoo.com>
2018-11-17 03:19:31 -0600

    Added double support

---
+++ double.cpp
@@ -0,0 +1,534 @@
+/******************************************************************************
+	Copyright 2018 Matthew Day
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+******************************************************************************/
+
+/******************************************************************************
+	File Name: double.cpp
+
+	Author: Matthew Day
+
+	Description:
+		Implementation file for double.h
+******************************************************************************/
+
+#include "double.h"
+
+namespace day {
+
+	// Assignment
+	Double& Double::operator=(Primitive &primitive) {
+
+		switch (primitive.getType()) {
+
+			case CHAR:
+
+				this->setDouble(primitive.getChar());
+				break;
+			case SHORT:
+
+				this->setDouble(primitive.getShort());
+				break;
+			case INTEGER:
+
+				this->setDouble(primitive.getInt());
+				break;
+			case LONG:
+
+				this->setDouble(primitive.getLong());
+				break;
+			case FLOAT:
+
+				this->setDouble(primitive.getFloat());
+				break;
+			case DOUBLE:
+
+				this->setDouble(primitive.getDouble());
+				break;
+			default:
+
+				throw new exception("Unsupported operation");
+		};
+
+		return *this;
+	}
+
+	// Addition
+	Double Double::operator+(Primitive &primitive) {
+
+		Double result;
+
+		switch (primitive.getType()) {
+
+			case CHAR:
+
+				result.setDouble(this->getDouble() + primitive.getChar());
+				break;
+			case SHORT:
+
+				result.setDouble(this->getDouble() + primitive.getShort());
+				break;
+			case INTEGER:
+
+				result.setDouble(this->getDouble() + primitive.getInt());
+				break;
+			case LONG:
+
+				result.setDouble(this->getDouble() + primitive.getLong());
+				break;
+			case FLOAT:
+
+				result.setDouble(this->getDouble() + primitive.getFloat());
+				break;
+			case DOUBLE:
+
+				result.setDouble(this->getDouble() + primitive.getDouble());
+				break;
+			default:
+
+				throw new exception("Unsupported operation");
+		};
+
+		return result;
+	}
+
+	// Subtraction
+	Double Double::operator-(Primitive &primitive) {
+
+		Double result;
+
+		switch (primitive.getType()) {
+
+			case CHAR:
+
+				result.setDouble(this->getDouble() - primitive.getChar());
+				break;
+			case SHORT:
+
+				result.setDouble(this->getDouble() - primitive.getShort());
+				break;
+			case INTEGER:
+
+				result.setDouble(this->getDouble() - primitive.getInt());
+				break;
+			case LONG:
+
+				result.setDouble(this->getDouble() - primitive.getLong());
+				break;
+			case FLOAT:
+
+				result.setDouble(this->getDouble() - primitive.getFloat());
+				break;
+			case DOUBLE:
+
+				result.setDouble(this->getDouble() - primitive.getDouble());
+				break;
+			default:
+
+				throw new exception("Unsupported operation");
+		};
+
+		return result;
+	}
+
+	// Multiplication
+	Double Double::operator*(Primitive &primitive) {
+
+		Double result;
+
+		switch (primitive.getType()) {
+
+			case CHAR:
+
+				result.setDouble(this->getDouble() * primitive.getChar());
+				break;
+			case SHORT:
+
+				result.setDouble(this->getDouble() * primitive.getShort());
+				break;
+			case INTEGER:
+
+				result.setDouble(this->getDouble() * primitive.getInt());
+				break;
+			case LONG:
+
+				result.setDouble(this->getDouble() * primitive.getLong());
+				break;
+			case FLOAT:
+
+				result.setDouble(this->getDouble() * primitive.getFloat());
+				break;
+			case DOUBLE:
+
+				result.setDouble(this->getDouble() * primitive.getDouble());
+				break;
+			default:
+
+				throw new exception("Unsupported operation");
+		};
+
+		return result;
+	}
+
+	// Division
+	Double Double::operator/(Primitive &primitive) {
+
+		Double result;
+
+		switch (primitive.getType()) {
+
+			case CHAR:
+
+				result.setDouble(this->getDouble() / primitive.getChar());
+				break;
+			case SHORT:
+
+				result.setDouble(this->getDouble() / primitive.getShort());
+				break;
+			case INTEGER:
+
+				result.setDouble(this->getDouble() / primitive.getInt());
+				break;
+			case LONG:
+
+				result.setDouble(this->getDouble() / primitive.getLong());
+				break;
+			case FLOAT:
+
+				result.setDouble(this->getDouble() / primitive.getFloat());
+				break;
+			case DOUBLE:
+
+				result.setDouble(this->getDouble() / primitive.getDouble());
+				break;
+			default:
+
+				throw new exception("Unsupported operation");
+		};
+
+		return result;
+	}
+
+	// Prefix incrementation
+	Double& Double::operator++() {
+
+		this->data++;
+
+		return *this;
+	}
+
+	// Postfix incrementation
+	Double Double::operator++(int) {
+
+		Double result = *this;
+		++*this;
+
+		return result;
+	}
+
+	// Prefix decrementation
+	Double& Double::operator--() {
+
+		this->data--;
+
+		return *this;
+	}
+
+	// Postfix decrementation
+	Double Double::operator--(int) {
+
+		Double result = *this;
+		--*this;
+
+		return result;
+	}
+
+	// Equals
+	bool Double::operator==(Primitive &primitive) {
+
+		bool result;
+
+		switch (primitive.getType()) {
+
+			case CHAR:
+
+				result = this->getDouble() == primitive.getChar();
+				break;
+			case SHORT:
+
+				result = this->getDouble() == primitive.getShort();
+				break;
+			case INTEGER:
+
+				result = this->getDouble() == primitive.getInt();
+				break;
+			case LONG:
+
+				result = this->getDouble() == primitive.getLong();
+				break;
+			case FLOAT:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() == primitive.getFloat();
+				break;
+			case DOUBLE:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() == primitive.getDouble();
+				break;
+			default:
+
+				throw new exception("Unsupported operation");
+		};
+
+		return result;
+	}
+
+	// Not equal
+	bool Double::operator!=(Primitive &primitive) {
+
+		bool result;
+
+		switch (primitive.getType()) {
+
+			case CHAR:
+
+				result = this->getDouble() != primitive.getChar();
+				break;
+			case SHORT:
+
+				result = this->getDouble() != primitive.getShort();
+				break;
+			case INTEGER:
+
+				result = this->getDouble() != primitive.getInt();
+				break;
+			case LONG:
+
+				result = this->getDouble() != primitive.getLong();
+				break;
+			case FLOAT:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() != primitive.getFloat();
+				break;
+			case DOUBLE:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() != primitive.getDouble();
+				break;
+			default:
+
+				throw new exception("Unsupported operation");
+		};
+
+		return result;
+	}
+
+	// Greater than
+	bool Double::operator>(Primitive &primitive) {
+
+		bool result;
+
+		switch (primitive.getType()) {
+
+			case CHAR:
+
+				result = this->getDouble() > primitive.getChar();
+				break;
+			case SHORT:
+
+				result = this->getDouble() > primitive.getShort();
+				break;
+			case INTEGER:
+
+				result = this->getDouble() > primitive.getInt();
+				break;
+			case LONG:
+
+				result = this->getDouble() > primitive.getLong();
+				break;
+			case FLOAT:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() > primitive.getFloat();
+				break;
+			case DOUBLE:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() > primitive.getDouble();
+				break;
+			default:
+
+				throw new exception("Unsupported operation");
+		};
+
+		return result;
+	}
+
+	// Less than
+	bool Double::operator<(Primitive &primitive) {
+
+		bool result;
+
+		switch (primitive.getType()) {
+
+			case CHAR:
+
+				result = this->getDouble() < primitive.getChar();
+				break;
+			case SHORT:
+
+				result = this->getDouble() < primitive.getShort();
+				break;
+			case INTEGER:
+
+				result = this->getDouble() < primitive.getInt();
+				break;
+			case LONG:
+
+				result = this->getDouble() < primitive.getLong();
+				break;
+			case FLOAT:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() < primitive.getFloat();
+				break;
+			case DOUBLE:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() < primitive.getDouble();
+				break;
+			default:
+
+				throw new exception("Unsupported operation");
+		};
+
+		return result;
+	}
+
+	// Greater than or equal to
+	bool Double::operator>=(Primitive &primitive) {
+
+		bool result;
+
+		switch (primitive.getType()) {
+
+			case CHAR:
+
+				result = this->getDouble() >= primitive.getChar();
+				break;
+			case SHORT:
+
+				result = this->getDouble() >= primitive.getShort();
+				break;
+			case INTEGER:
+
+				result = this->getDouble() >= primitive.getInt();
+				break;
+			case LONG:
+
+				result = this->getDouble() >= primitive.getLong();
+				break;
+			case FLOAT:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() >= primitive.getFloat();
+				break;
+			case DOUBLE:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() >= primitive.getDouble();
+				break;
+			default:
+
+				throw new exception("Unsupported operation");
+		};
+
+		return result;
+	}
+
+	// Less than or equal to
+	bool Double::operator<=(Primitive &primitive) {
+
+		bool result;
+
+		switch (primitive.getType()) {
+
+			case CHAR:
+
+				result = this->getDouble() <= primitive.getChar();
+				break;
+			case SHORT:
+
+				result = this->getDouble() <= primitive.getShort();
+				break;
+			case INTEGER:
+
+				result = this->getDouble() <= primitive.getInt();
+				break;
+			case LONG:
+
+				result = this->getDouble() <= primitive.getLong();
+				break;
+			case FLOAT:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() <= primitive.getFloat();
+				break;
+			case DOUBLE:
+
+				// TODO: Test if this is possible with the issues caused by it's precision
+				result = this->getDouble() <= primitive.getDouble();
+				break;
+			default:
+
+				throw new exception("Unsupported operation");
+		};
+
+		return result;
+	}
+
+	// Addition assignment
+	Double& Double::operator+=(Primitive &primitive) {
+
+		*this = (Primitive&)(*this + primitive);
+
+		return *this;
+	}
+
+	// Subtraction assignment
+	Double& Double::operator-=(Primitive &primitive) {
+
+		*this = (Primitive&)(*this - primitive);
+
+		return *this;
+	}
+
+	// Multiplication assignment
+	Double& Double::operator*=(Primitive &primitive) {
+
+		*this = (Primitive&)(*this * primitive);
+
+		return *this;
+	}
+
+	// Division assignment
+	Double& Double::operator/=(Primitive &primitive) {
+
+		*this = (Primitive&)(*this / primitive);
+
+		return *this;
+	}
+}
---
+++ double.h
@@ -0,0 +1,327 @@
+/******************************************************************************
+	Copyright 2018 Matthew Day
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+******************************************************************************/
+
+/******************************************************************************
+	File Name: double.h
+
+	Author: Matthew Day
+
+	Class Name: Double
+
+	Description:
+		Wrapper class for double
+******************************************************************************/
+
+#pragma once
+
+#include "primitives.h"
+
+namespace day {
+
+	class Double : public Primitive {
+
+	private:
+
+		double data;
+
+		Double() : Primitive(Type::DOUBLE), data(0) {}
+	public:
+
+		/******************************************************************************
+		Constructor
+		******************************************************************************/
+		Double(double data) : Primitive(Type::DOUBLE), data(data) {}
+
+		// Getter
+		inline double getDouble() override {
+
+			return data;
+		}
+
+		// Setter
+		inline void setDouble(double data) override {
+
+			this->data = data;
+		}
+
+		/******************************************************************************
+			Arithmetic operators
+		******************************************************************************/
+
+		/******************************************************************************
+			Function Name: operator=
+
+			Des:
+				Override assignment operator
+
+			Params:
+				primitive - type Primitive &, the primitive type this is to be
+					assigned to
+
+			Returns:
+				type Double &, the resulting value
+		******************************************************************************/
+		Double& operator=(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator+
+
+			Des:
+				Override addition operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be added to
+
+			Returns:
+				type Double, the resulting value
+		******************************************************************************/
+		Double operator+(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator-
+
+			Des:
+				Override subtraction operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be subtracted by
+
+			Returns:
+				type Double, the resulting value
+		******************************************************************************/
+		Double operator-(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator*
+
+			Des:
+				Override multiplication operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be multiplied by
+
+			Returns:
+				type Double &, the resulting value
+		******************************************************************************/
+		Double operator*(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator/
+
+			Des:
+				Override division operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be divided by
+
+			Returns:
+				type Double, the resulting value
+		******************************************************************************/
+		Double operator/(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator++
+
+			Des:
+				Override prefix increment operator
+
+			Returns:
+				type Double &, the resulting value
+		******************************************************************************/
+		Double& operator++();
+
+		/******************************************************************************
+			Function Name: operator++
+
+			Des:
+				Override postfix increment operator
+
+			Returns:
+				type Double, the resulting value
+		******************************************************************************/
+		Double operator++(int);
+
+		/******************************************************************************
+			Function Name: operator--
+
+			Des:
+				Override prefix decrement operator
+
+			Returns:
+				type Double &, the resulting value
+		******************************************************************************/
+		Double& operator--();
+
+		/******************************************************************************
+			Function Name: operator--
+
+			Des:
+				Override postfix decrement operator
+
+			Returns:
+				type Double, the resulting value
+		******************************************************************************/
+		Double operator--(int);
+
+		/******************************************************************************
+			Comparison operators
+		******************************************************************************/
+
+		/******************************************************************************
+			Function Name: operator==
+
+			Des:
+				Override equals operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be compared to
+
+			Returns:
+				type bool, the resulting value
+		******************************************************************************/
+		bool operator==(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator!=
+
+			Des:
+				Override not equal operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be compared to
+
+			Returns:
+				type bool, the resulting value
+		******************************************************************************/
+		bool operator!=(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator>
+
+			Des:
+				Override greater than operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be compared to
+
+			Returns:
+				type bool, the resulting value
+		******************************************************************************/
+		bool operator>(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator<
+
+			Des:
+				Override less than operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be compared to
+
+			Returns:
+				type bool, the resulting value
+		******************************************************************************/
+		bool operator<(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator>=
+
+			Des:
+				Override greater than or equal to operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be compared to
+
+			Returns:
+				type bool, the resulting value
+		******************************************************************************/
+		bool operator>=(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator<=
+
+			Des:
+				Override less than or equal to operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be compared to
+
+			Returns:
+				type bool, the resulting value
+		******************************************************************************/
+		bool operator<=(Primitive &primitive);
+
+		/******************************************************************************
+			Compound assignment operators
+		******************************************************************************/
+
+		/******************************************************************************
+			Function Name: operator+=
+
+			Des:
+				Override addition assignment operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be added to
+
+			Returns:
+				type Double &, the resulting value
+		******************************************************************************/
+		Double& operator+=(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator-=
+
+			Des:
+				Override subtraction assignment operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be subtracted by
+
+			Returns:
+				type Double &, the resulting value
+		******************************************************************************/
+		Double& operator-=(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator*=
+
+			Des:
+				Override multiplication assignment operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be multiplied by
+
+			Returns:
+				type Double &, the resulting value
+		******************************************************************************/
+		Double& operator*=(Primitive &primitive);
+
+		/******************************************************************************
+			Function Name: operator/=
+
+			Des:
+				Override division assignment operator
+
+			Params:
+				primitive - type Primitive &, the primitive type to be divided by
+
+			Returns:
+				type Double &, the resulting value
+		******************************************************************************/
+		Double& operator/=(Primitive &primitive);
+	};
+}
