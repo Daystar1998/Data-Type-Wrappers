5d29a7c65cb131ef6b3c2147de4598e78079747a
Matthew Day <daystar.1998@yahoo.com>
2018-11-23 16:15:50 -0600

    Update bool test cases

--- Data-Type-Wrappers/Tests/boolTests.cpp
+++ Data-Type-Wrappers/Tests/boolTests.cpp
@@ -9,6 +9,38 @@ void runBoolTests() {
 	primBool2 = make_shared<Boolean>(false);
 
 	/******************************************************************************
+		Bitwise NOT Test
+	******************************************************************************/
+	if ((~*primBool1)->getBool())
+		cout << "Bool Bitwise NOT Test: Success" << endl;
+	else
+		cout << "Bool Bitwise NOT Test: Failed" << endl;
+
+	/******************************************************************************
+		Bitwise AND Test
+	******************************************************************************/
+	if ((*primBool1 & *primBool2)->getBool())
+		cout << "Bool Bitwise AND Test: Failed" << endl;
+	else
+		cout << "Bool Bitwise AND Test: Success" << endl;
+
+	/******************************************************************************
+		Bitwise OR Test
+	******************************************************************************/
+	if ((*primBool1 | *primBool2)->getBool())
+		cout << "Bool Bitwise OR Test: Success" << endl;
+	else
+		cout << "Bool Bitwise OR Test: Failed" << endl;
+
+	/******************************************************************************
+		Bitwise XOR Test
+	******************************************************************************/
+	if ((*primBool1 ^ *primBool2)->getBool())
+		cout << "Bool Bitwise XOR Test: Success" << endl;
+	else
+		cout << "Bool Bitwise XOR Test: Failed" << endl;
+
+	/******************************************************************************
 		Logical NOT Test
 	******************************************************************************/
 	if ((!*primBool1)->getBool())
